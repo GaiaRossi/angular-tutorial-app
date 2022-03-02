2a817db551995fb3f3a068c809946990dee0a6aa
GaiaRossi <gaia.rossi@edu.unife.it>
2022-03-02 09:29:30 +0100

    Created a Hero editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+/**per poter usare ngModel devo importare FormsModule */
+import { FormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
+  /**devo aggiungerlo anche qui cosi lo importa */
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
